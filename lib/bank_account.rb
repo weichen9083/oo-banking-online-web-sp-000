f27994efc40dcfce0ab3ec5078f6a79a17aa1f43
wei chen <weichen9083@gmail.com>
2020-12-08 21:19:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,32 +1,24 @@
 class BankAccount
   
-attr_accessor :balance, :status
-  attr_reader :name
-
+  
+  attr_accessor  :balance, :status 
+  attr_reader :name 
+  
   def initialize(name)
-    @name = name
+    @name  = name 
     @balance = 1000
     @status = "open"
-  end
-
+  end 
+  
   def deposit(amount)
-    @balance += amount
-  end
+    
+    self.balance += amount 
+    
+  end 
+  
+  def display_balance 
+    self.balance
+  end 
 
-  def display_balance
-    "Your balance is $#{@balance}."
-  end
-
-  def valid?
-    if @status == "open" && @balance > 0
-      true
-    else
-      false
-    end
-  end
-
-  def close_account
-    @status = "closed"
-  end
 
 end
